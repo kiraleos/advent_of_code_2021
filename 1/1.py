84c0e7a10c35b723a3cd54a9f401385539638172
kiraleos <okiraleos@gmail.com>
2021-12-01 13:26:15 +0200

    day 1, part 2

--- 1/1.py
+++ 1/1.py
@@ -8,5 +8,20 @@ with open("input") as f:
             print(lines[i], lines[i + 1])
             ctr += 1
 
+    print(ctr)
 
-print(ctr)
+# part 2
+with open("input") as f:
+    lines = f.readlines()
+    ctr = 0
+    lines = [int(lines[i]) for i in range(len(lines))]
+    windows = []
+    for i in range(len(lines)):
+        windows.append(lines[i : i + 3])
+    for i in range(len(windows)):
+        if len(windows[i]) == 3 and len(windows[i + 1]) == 3:
+            if sum(windows[i + 1]) > sum(windows[i]):
+                print(windows[i + 1], windows[i])
+                ctr += 1
+
+    print(ctr)
